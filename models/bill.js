3ed2f74c6aa19de88c1276e005d7f1acd398938c
Frank Cruz <frank18cr@gmail.com>
2017-05-01 16:07:45 -0600

    adding bills controller, model and route handler

--- models/bill.js
+++ models/bill.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var billSchema = new Schema({
   period: {type: String, required: true},
   provider: {type: String, required: true},
-  consumption: {type: Number, required: true},
+  consumption: {type: String, required: true},
   cost: {type: String, required: true},
   comments: String
 });
